1505f0fa2f1ecdd46b6761e561327ee3e6225ed4
durga.p <durga.p@flipkart.com>
2016-02-07 00:36:59 +0530

    WIP

--- src/test/java/com/solvevolve/atp/test/CopyConstructorTest.java
+++ src/test/java/com/solvevolve/atp/test/CopyConstructorTest.java
@@ -1,13 +1,43 @@
 package com.solvevolve.atp.test;
 
+import com.google.testing.compile.JavaFileObjects;
+
+import com.solvevolve.atp.CopyConstructorAnnotationProcessor;
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
 
-public class CopyConstructorTest {
+import javax.tools.JavaFileObject;
+
+import static com.google.common.truth.Truth.assertAbout;
+import static com.google.testing.compile.JavaSourceSubjectFactory.javaSource;
+
+public class BuilderAnnotationTest {
 
   @Test
-  public void test() {
-    assertEquals("one","one");
+  public void processorTest() {
+    String code = "package test;\n"
+                  + "import com.solvevolve.atp.CopyConstructor;\n"
+                  + "@CopyConstructor\n"
+                  + "public class User {\n"
+                  + "  private String name;\n"
+                  + "  private String emailId;\n"
+                  + "\n"
+                  + "  public static void main(String[] args) {\n"
+                  + "    System.out.println(\"Hello\");\n"
+                  + "    User user = new User();\n"
+                  + "  }\n"
+                  + "}\n";
+
+    JavaFileObject inputFile = JavaFileObjects.forSourceString("User", code);
+
+    Collection<CopyConstructorAnnotationProcessor>
+        processors =
+        Arrays.asList(new CopyConstructorAnnotationProcessor());
+
+
+    assertAbout(javaSource()).that(inputFile).processedWith(processors).compilesWithoutError();
   }
 }
