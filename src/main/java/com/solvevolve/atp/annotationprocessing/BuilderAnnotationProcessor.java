62789d3e04e7c28d753edbce9b10ef99b9b45a11
durga.p <durga.p@flipkart.com>
2016-02-09 22:27:34 +0530

    basic commit

--- src/main/java/com/solvevolve/atp/annotationprocessing/BuilderAnnotationProcessor.java
+++ src/main/java/com/solvevolve/atp/annotationprocessing/BuilderAnnotationProcessor.java
@@ -1,43 +1,104 @@
 package com.solvevolve.atp.annotationprocessing;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.Sets;
 
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+import javax.tools.Diagnostic;
 
 @SupportedAnnotationTypes(
     "com.solvevolve.atp.annotationprocessing.Builder"
 )
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
 @AutoService(Processor.class)
 public class BuilderAnnotationProcessor extends AbstractProcessor {
 
+  private Filer filer;
+  private Messager messager;
+  private Types typeUtils;
+  private Elements elementUtils;
+
   @Override
   public synchronized void init(ProcessingEnvironment processingEnv) {
-
+    this.filer = processingEnv.getFiler();
+    this.messager = processingEnv.getMessager();
+    this.typeUtils = processingEnv.getTypeUtils();
+    this.elementUtils = processingEnv.getElementUtils();
   }
 
   @Override
   public boolean process(Set<? extends TypeElement> annotations,
                          RoundEnvironment roundEnv) {
+    if (annotations.size() == 0) {
+      return false;
+    } else {
+      TypeElement builderAnnotation = annotations.iterator().next();
+      Set<? extends Element>
+          elementsAnnotated =
+          roundEnv.getElementsAnnotatedWith(builderAnnotation);
+      for (Element element : elementsAnnotated) {
+        BuilderModel model = getModel(element);
+        processModel(model);
+      }
+    }
     return false;
   }
 
-  @Override
-  public Set<String> getSupportedAnnotationTypes() {
-    return Sets.newHashSet(
-        "com.solvevolve.atp.annotationprocessing.Builder");
+  private void processModel(BuilderModel model) {
+    System.out.println("creating file");
+
   }
 
-  @Override
-  public SourceVersion getSupportedSourceVersion() {
-    return SourceVersion.latestSupported();
+  private BuilderModel getModel(Element rootElement) {
+    if (rootElement.getKind() == ElementKind.CLASS
+        || rootElement.getEnclosingElement().getKind() == ElementKind.PACKAGE) {
+      BuilderModel builderModel = new BuilderModel();
+
+      TypeElement typeElement = (TypeElement) rootElement;
+      PackageElement packageElement = (PackageElement) typeElement.getEnclosingElement();
+
+      builderModel.setClassName(rootElement.getSimpleName().toString());
+      builderModel.setPackageName(packageElement.getQualifiedName().toString());
+
+      for (Element element : rootElement.getEnclosedElements()) {
+        if (element.getKind() == ElementKind.METHOD && element.getSimpleName().toString()
+            .startsWith("set")) {
+          ExecutableElement executableElement = (ExecutableElement) element;
+          executableElement.getEnclosedElements();
+          if (executableElement.getReturnType().getKind() == TypeKind.VOID
+              && executableElement.getParameters().size() == 1) {
+            VariableElement variableElement = executableElement.getParameters().get(0);
+            String variableName = variableElement.getSimpleName().toString();
+            DeclaredType declaredType = (DeclaredType) variableElement.asType();
+            builderModel
+                .addSetFuncMetadata(new BuilderModel.SetFuncMetadata(variableName, declaredType));
+
+          }
+        }
+      }
+
+    } else {
+      this.messager.printMessage(Diagnostic.Kind.ERROR, "Applicable only to top level Classes");
+    }
+    return null;
   }
 }
